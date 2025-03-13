44798ff16b8d230622b5d0ea0ed1be70a78b9f77
richard-nagy <big.ricsoo@gmail.com>
2025-03-13 21:01:33 +0100

    #2: Implement MUI

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 The goal of this project is to create a Job list manager app. Which fetches and displays the data.
 ### v0.1 Development Goals:
  - [ ] Implement react-router
- - [ ] Implement MUI
+ - [x] Implement MUI
  - [ ] Implement Redux
  - [ ] Add a *public/data.json* file as a mocked fetch source
  - [ ] Fetch the data from the *public/data.json* file 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
-    return <>
-        joblistings
-    </>;
-}
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import { CssBaseline } from "@mui/material";
+
+const App = () => {
+    return <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>JobListing</h1>
+    </ThemeProvider>;
+};
 
 export default App;
